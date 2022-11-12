74c2c6d363d9c74103b6741e22dfaafb4dc2876f
Clint Mukarakate <clintroymkt@gmail.com>
2022-11-12 23:42:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/connquest/daily_tasks_activity.java
@@ -0,0 +1,78 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		daily_tasks
+	 *	@date 		Friday 11th of November 2022 02:34:01 AM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.example.connquest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.connquest.R;
+
+	public class daily_tasks_activity extends Activity {
+
+	
+	private View _bg__daily_tasks_ek2;
+	private ImageView vector_1;
+	private TextView let_s_see_what_kind_of_quests_we_can_tackle_today__i_m_sure_you_ll_do_your_best_;
+	private View rectangle_2;
+	private View rectangle_4;
+	private TextView score__20;
+	private View rectangle_3;
+	private TextView quest_1;
+	private ImageView star_1;
+	private ImageView star_2;
+	private ImageView star_3;
+	private TextView quest_2;
+	private TextView quest_3;
+	private TextView score__25;
+	private TextView score__0;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.daily_tasks);
+
+		
+		_bg__daily_tasks_ek2 = (View) findViewById(R.id._bg__daily_tasks_ek2);
+		vector_1 = (ImageView) findViewById(R.id.vector_1);
+		let_s_see_what_kind_of_quests_we_can_tackle_today__i_m_sure_you_ll_do_your_best_ = (TextView) findViewById(R.id.let_s_see_what_kind_of_quests_we_can_tackle_today__i_m_sure_you_ll_do_your_best_);
+		rectangle_2 = (View) findViewById(R.id.rectangle_2);
+		rectangle_4 = (View) findViewById(R.id.rectangle_4);
+		score__20 = (TextView) findViewById(R.id.score__20);
+		rectangle_3 = (View) findViewById(R.id.rectangle_3);
+		quest_1 = (TextView) findViewById(R.id.quest_1);
+		star_1 = (ImageView) findViewById(R.id.star_1);
+		star_2 = (ImageView) findViewById(R.id.star_2);
+		star_3 = (ImageView) findViewById(R.id.star_3);
+		quest_2 = (TextView) findViewById(R.id.quest_2);
+		quest_3 = (TextView) findViewById(R.id.quest_3);
+		score__25 = (TextView) findViewById(R.id.score__25);
+		score__0 = (TextView) findViewById(R.id.score__0);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
---
+++ app/src/main/java/com/example/connquest/start_screen_activity.java
@@ -0,0 +1,60 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		start_screen
+	 *	@date 		Friday 11th of November 2022 02:33:23 AM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.example.connquest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.connquest.R;
+
+	public class start_screen_activity extends Activity {
+
+	
+	private View _bg__start_screen_ek2;
+	private ImageView vector_1;
+	private View rectangle_2;
+	private TextView play_now;
+	private View rectangle_3;
+	private TextView about;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.start_screen);
+
+		
+		_bg__start_screen_ek2 = (View) findViewById(R.id._bg__start_screen_ek2);
+		vector_1 = (ImageView) findViewById(R.id.vector_1);
+		rectangle_2 = (View) findViewById(R.id.rectangle_2);
+		play_now = (TextView) findViewById(R.id.play_now);
+		rectangle_3 = (View) findViewById(R.id.rectangle_3);
+		about = (TextView) findViewById(R.id.about);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
